bd8ec46a5cf97de2b3043f49aaefd2100b1cbd76
brendan887 <mahbrendan@gmail.com>
2024-10-13 18:16:45 -0700

    Removed unnecessary files

--- models/front_back/cfg.py
+++
@@ -1,8 +0,0 @@
-NUM_EPOCHS = 10
-DATA_DIR = 'path_to_your_dataset'
-BATCH_SIZE = 32
-INFERENCE_BATCH_SIZE = 32
-NUM_WORKERS = 4
-LEARNING_RATE = 0.
-MOMENTUM = 0.9
-CLASS_NAMES = ['graded_front', 'graded_back', 'full_back', 'full_front', 'not_full']
